db286d70bc1abda4c370b96e19c63f4957c7bd86
Bhadru Nair <bhadrumaster@hotmail.com>
2023-12-10 12:11:24 +0530

    first commit

---
+++ src/components/Notification.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Notification = () => {
+  return (
+    <div className='h-12 bg-red-500 px-4 text-white flex items-center justify-center text-sm
+    text-center md:text-base cursor-pointer'>
+        Free Delivery for all orders
+    </div>
+  )
+}
+
+export default Notification
